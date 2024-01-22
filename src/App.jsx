579067552d4ad5f32f9909c2ab930438beec5b0c
Francesco <142682839+FraLobbia@users.noreply.github.com>
2024-01-22 14:33:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./assets/dist/css/style.min.css";
 import MyNavbar from "./components/MyNavbar";
+import Preview from "./components/profilePage/Preview";
 
 function App() {
-  return (
-    <div>
-      <MyNavbar />
-    </div>
-  );
+	return (
+		<div>
+			<MyNavbar />
+			<Preview />
+		</div>
+	);
 }
 
 export default App;

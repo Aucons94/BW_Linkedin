85c2d77a5ce65a514e2bb334614c9af201d0a1b8
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-01-22 11:16:51 +0100

    update

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -2,15 +2,23 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import linkedin from "../linkedin.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Form, InputGroup } from "react-bootstrap";
 
 function MyNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-white p-0">
       <Container>
         <Navbar.Brand href="https://as2.ftcdn.net/v2/jpg/03/96/56/19/1000_F_396561934_t1OwjMZHrVY211bHA5uUEAix0F1SyLci.jpg">
-          <img src={linkedin} alt="" width={30} className="primary" />
+          <FontAwesomeIcon icon={faLinkedin} style={{ color: "#0A66C2" }} size="2xl" />
         </Navbar.Brand>
+        <InputGroup>
+          <InputGroup.Text>
+            <i class="bi bi-search text-primary"></i>
+          </InputGroup.Text>
+          <Form.Control type="text" placeholder="Cerca" className=" mr-sm-2" />
+        </InputGroup>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">

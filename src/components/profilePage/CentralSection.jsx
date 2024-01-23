0277482d0b2e00d13dfc015be8e423fb32bb850a
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-01-23 09:55:49 +0100

    Update CentralSection.jsx

--- src/components/profilePage/CentralSection.jsx
+++ src/components/profilePage/CentralSection.jsx
@@ -3,25 +3,32 @@ import Preview from "./centralSection/Preview";
 import SidebarProfileRight from "./SidebarProfileRight";
 
 const CentralSection = () => {
-	return (
-		<Container className="d-flex align-items-start gap-2">
-			<Row>
-				<Col xs={12} md={7} lg={8} xl={9}>
-					<Preview />
-					{/* vari componenti */}
-					{/* vari componenti */}
-					{/* vari componenti */}
-					{/* vari componenti */}
-					{/* vari componenti */}
-					{/* vari componenti */}
-					{/* vari componenti */}
-					{/* vari componenti */}
-				</Col>
-				<Col md={5} lg={4} xl={3}>
-					<SidebarProfileRight />
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container className="d-flex align-items-start gap-2" style={{ maxWidth: "1200px" }}>
+      <Preview />
+      {/* sezione con img profilo, nome, domicilio etc */}
+      <SidebarProfileRight />
+    </Container>
+  );
+  return (
+    <Container className="d-flex align-items-start gap-2">
+      <Row>
+        <Col xs={12} md={7} lg={8} xl={9}>
+          <Preview />
+          {/* vari componenti */}
+          {/* vari componenti */}
+          {/* vari componenti */}
+          {/* vari componenti */}
+          {/* vari componenti */}
+          {/* vari componenti */}
+          {/* vari componenti */}
+          {/* vari componenti */}
+        </Col>
+        <Col md={5} lg={4} xl={3}>
+          <SidebarProfileRight />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 export default CentralSection;

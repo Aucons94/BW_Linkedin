94ab7322052b4c5ec85004bca436276d055cc864
Francesco <142682839+FraLobbia@users.noreply.github.com>
2024-01-24 19:05:09 +0100

    commenti fatti

--- src/components/HomePage/HomePosts.jsx
+++ src/components/HomePage/HomePosts.jsx
@@ -1,44 +1,111 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { token } from "../../token";
 import { useEffect } from "react";
-import { getPosts } from "../../redux/actions";
+import { getPosts, getPostsData } from "../../redux/actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faComment,
+	faEllipsis,
+	faPaperPlane,
+	faRetweet,
+	faThumbsUp,
+	faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import ButtonLink from "../profilePage/utility/ButtonLink";
 
 const HomePosts = () => {
 	const posts = useSelector((state) => state.postData.posts);
 	const dispatch = useDispatch();
 
-	const fetchPosts = async () => {
-		const endpoint = "https://striveschool-api.herokuapp.com/api/posts/";
-		try {
-			let response = await fetch(endpoint, {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: token,
-				},
-			});
-			let data = await response.json();
-			console.log(data);
-			dispatch(getPosts(data));
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
 	useEffect(() => {
-		fetchPosts();
-		console.log(posts);
+		dispatch(getPostsData());
+		console.log("Singolo post: ", posts[5]);
 	}, []);
+
+	const sortedPosts = [...posts].sort(
+		(a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+	);
+
+	function timeAgo(dateString) {
+		const currentDate = new Date();
+		const pastDate = new Date(dateString);
+
+		const timeDifference = currentDate - pastDate;
+		const minutes = Math.floor(timeDifference / (1000 * 60));
+
+		if (minutes < 1) {
+			return "Ora";
+		} else if (minutes === 1) {
+			return "1 minuto fa";
+		} else if (minutes < 60) {
+			return `${minutes} minuti fa`;
+		} else if (minutes < 60 * 24) {
+			const hours = Math.floor(minutes / 60);
+			return `${hours} ${hours === 1 ? "ora" : "ore"} fa`;
+		} else {
+			const days = Math.floor(minutes / (60 * 24));
+			return `${days} ${days === 1 ? "giorno" : "giorni"} fa`;
+		}
+	}
+
+	const dateString = "2024-01-24T17:38:56.209Z";
+	const timeAgoString = timeAgo(dateString);
+
 	return (
 		<>
-			{posts.map((post) => {
+			{sortedPosts.map((post) => {
 				return (
-					<Card className="my-2 p-5">
-						<h3>
-							{post.user.name} {post.user.surname}
-						</h3>
-						<p className="text-primary">{post.text}</p>
+					<Card key={post._id} className="my-2 p-3 gap-3">
+						<div className="d-flex">
+							<img
+								src={
+									post.user.image
+										? post.user.image
+										: "https://m.media-amazon.com/images/I/51zf0DXxT-L._AC_UF1000,1000_QL80_.jpg"
+								}
+								alt="profilo"
+								style={{
+									width: "70px",
+									height: "70px",
+									borderRadius: "50%",
+								}}
+							/>
+							<div className="ms-3">
+								<h5 className="m-0">
+									{post.user.name} {post.user.surname}
+								</h5>
+								<p className="m-0">{post.user.title}</p>
+								<p>{timeAgo(post.createdAt)}</p>
+							</div>
+							<div className="ms-auto fs-4 text-secondary">
+								<FontAwesomeIcon
+									className="me-3"
+									icon={faEllipsis}
+								/>
+								<FontAwesomeIcon icon={faXmark} />
+							</div>
+						</div>
+						<p className="">{post.text}</p>
+						<hr />
+						<div className="d-flex justify-content-around">
+							<ButtonLink className={"text-secondary"}>
+								<FontAwesomeIcon icon={faThumbsUp} />
+								<span className="ms-2">Consiglia</span>
+							</ButtonLink>
+							<ButtonLink className={"text-secondary"}>
+								<FontAwesomeIcon icon={faComment} />
+								<span className="ms-2">Commenta</span>
+							</ButtonLink>
+							<ButtonLink className={"text-secondary"}>
+								<FontAwesomeIcon icon={faRetweet} />
+								<span className="ms-2">Diffondi il post</span>
+							</ButtonLink>
+							<ButtonLink className={"text-secondary"}>
+								<FontAwesomeIcon icon={faPaperPlane} />
+								<span className="ms-2">Invia</span>
+							</ButtonLink>
+						</div>
 					</Card>
 				);
 			})}
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,13 @@
+import { token } from "../../token";
+
 export const NAVBAR_INFO = "NAVBAR_INFO";
 export const GET_PROFILI = "GET_PROFILI";
 export const GET_EXPERIENCE = "GET_EXPERIENCE";
 export const CREATE_POST = "CREATE_POST";
 export const GET_POST = "GET_POST";
+export const SET_LOADING_TRUE = "SET_LOADING_TRUE";
+export const SET_LOADING_FALSE = "SET_LOADING_FALSE";
+export const SET_ERROR = "SET_ERROR";
 
 export const setNavbarInfo = (payload) => ({
 	type: NAVBAR_INFO,
@@ -28,3 +33,37 @@ export const getPosts = (payload) => ({
 	type: GET_POST,
 	payload,
 });
+
+export const getPostsData = () => {
+	return async (dispatch, getState) => {
+		try {
+			dispatch({
+				type: SET_LOADING_TRUE,
+			});
+			// const query = getState().music.query;
+			const endpoint =
+				"https://striveschool-api.herokuapp.com/api/posts/";
+			const resp = await fetch(endpoint, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: token,
+				},
+			});
+			if (!resp.ok) {
+				throw "Errore nella fetch";
+			}
+			const response = await resp.json();
+			console.log(response);
+			dispatch(getPosts(response));
+			dispatch({
+				type: SET_LOADING_FALSE,
+			});
+		} catch (error) {
+			dispatch({
+				type: SET_ERROR,
+				payload: error,
+			});
+		}
+	};
+};
